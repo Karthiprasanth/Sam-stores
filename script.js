1d8b3fc1e877730df8c6e8b5d2cbc83b0ca57ddd
Karthiprasanth <131578863+Karthiprasanth@users.noreply.github.com>
2025-11-24 18:01:18 +0530

    Final commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var sidenav = document.querySelector(".side-navbar")
+
+
+//this are the functions to open and close side navbar//
+function openNav(){
+   sidenav.style.left = "0"
+}
+
+function closeNav(){
+   sidenav.style.left = "-50%"
+}
